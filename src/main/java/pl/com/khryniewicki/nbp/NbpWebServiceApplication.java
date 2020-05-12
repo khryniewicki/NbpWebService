c583fbb9d754e293d4c908dddacb0abb2c1c715b
Konrad <konrad.hryniewicki@gmail.com>
2020-05-12 13:01:47 +0200

    - xsd loaded classes

---
+++ src/main/java/pl/com/khryniewicki/nbp/NbpWebServiceApplication.java
@@ -0,0 +1,12 @@
+package pl.com.khryniewicki.nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbpWebServiceApplication {
+
+    public static void main(String[] args)  {
+        SpringApplication.run(pl.com.khryniewicki.nbp.NbpWebServiceApplication.class, args);
+    }
+}
